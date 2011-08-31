08fe348bc74b18c61c2256ada4f0c10c15385c87
Michael Schmidt <michael.j.schmidt@gmail.com>
2011-08-31 11:19:14 +0200

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saptunes::Application.config.secret_token = 'f41e25d306080d272f02ffd7d180d1076ff974080c0179831d71c0c21f1c6a7acdfb358e68a322a0e9ae264c72aba37da65893e105e2916421fa034aabbc23c7'
